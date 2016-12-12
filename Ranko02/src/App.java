b0d4a5de2c4f28df8adcefd600ca6853d06de8f0
Mercury2001 <vujicicr@yahoo.com>
2016-12-12 15:08:50 +0100

    drugi primjer

---
+++ Ranko02/src/App.java
@@ -0,0 +1,28 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		/** definisanje nekoliko varijabli, razlicitih tipova
+		 * 
+		 * integer, char,byte 
+		 */
+		
+				int myNumber = 88;
+				char myChar = 'R';
+				byte myByte = 127;
+				
+		/**
+		 * ispisacemo definisane vrijednosti u konzolu
+		 */
+				System.out.println(myNumber);
+				System.out.println(myChar);
+				System.out.println(myByte);
+		
+				
+		 
+		// TODO Auto-generated method stub
+
+	}
+
+}
