d3c081e36cad7a31904bf61a6817328a4822e399
Ranko <Ranko>
2016-12-13 10:37:29 +0100

    Tri WHILE ciklusa

---
+++ Ranko04 - WHILE ciklusi/src/App.java
@@ -0,0 +1,43 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		/*// TODO Auto-generated method stub
+		int value = 0;
+	    
+        while(value < 10)
+        {
+            System.out.println("Iteracija broj: " + value);
+            
+            value = value + 1;
+        }
+        */
+		int value = 0;
+		
+		while(value < 100)
+        {
+            System.out.println("Iteracija broj: " + value);
+            
+            value = value + 1;	
+        }
+		
+int vrijed = 30;
+		
+		while(vrijed < 50)
+        {
+            System.out.println("Iteracija broj: " + vrijed);
+            
+            vrijed = vrijed + 1;	
+        }
+int vr = 50;
+		
+		while(vr > 30)
+        {
+            System.out.println("Iteracija broj: " + vr);
+            
+            vr = vr - 1;	
+        }
+	
+	}
+		
+}
